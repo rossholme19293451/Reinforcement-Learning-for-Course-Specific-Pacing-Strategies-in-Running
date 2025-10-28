8ee14bb6a2592bbd7a28c7d0ca97011e82ecfe33
rossholme19293451 <19293451@brookes.ac.uk>
2025-10-28 13:02:45 +0000

    Elevation Profile Creator

--- env/hybrid_keller_env.py
+++ env/hybrid_keller_env.py
@@ -1,7 +1,26 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import torch
-import gym
+import gymnasium as gym
+
+
 
 #create the environment
-env = gym.make('Pacman-v0')
+env = gym.make('HybridKeller-v0')
+env.reset()
+
+# play 10 games
+for i in range(10):
+    # initialize the variables
+    done = False
+    game_rew = 0
+    while not done:
+        # choose a random action
+        action = env.action_space.sample()
+        # take a step in the environment
+        new_obs, rew, done, truncated, info = env.step(action)
+        game_rew += rew
+        # when is done, print the cumulative reward of the game and reset the environment
+        if done or truncated:
+            print('Episode %d finished, reward:%d' % (i, game_rew))
+            env.reset()
---
+++ experiments/elevation_profile_creator.py
@@ -0,0 +1,61 @@
+import gpxpy
+import numpy as np
+from scipy.interpolate import interp1d
+from scipy.signal import savgol_filter
+
+#takes gpx file, returns array of distance and elevation at that point
+def parse_GPX_to_points(gpx):
+    points = []
+    for track in gpx.tracks:
+        for segment in track.segments:
+            total_distance = 0
+            prev = None
+            for point in segment.points:
+                if prev is not None:
+                    dist = point.distance_3d(prev)
+                    total_distance += dist
+
+                points.append((total_distance, point.elevation))
+                prev = point
+
+    points = np.array(points, dtype=float)
+    dists, idx = np.unique(points[:, 0], return_index=True)
+    elevs = points[idx, 1]
+    return dists, elevs
+
+#interpolates 1m steps between the distance and elevation points
+def resample_to_1m(dists, elevs, step=1.0):
+    elevs = savgol_filter(elevs, window_length=7, polyorder=5)
+    interp_func = interp1d(dists, elevs, kind='linear', fill_value='extrapolate')
+    new_dists = np.arange(0, dists[-1], step=step)
+    new_elevs = interp_func(new_dists)
+    return new_dists, new_elevs
+
+if __name__ == "__main__":
+    # load gpx file
+    with open("../data/course_GPXs/Ryde_10.gpx", "r") as f:
+        gpx = gpxpy.parse(f)
+
+    dists, elevs = parse_GPX_to_points(gpx)
+    new_dists, new_elevs = resample_to_1m(dists, elevs)
+
+    import matplotlib.pyplot as plt
+
+    plt.figure(figsize=(12, 4))
+    plt.plot(new_dists, new_elevs, color='blue', label='Interpolated Elevation (1m)')
+    plt.xlabel('Distance (m)')
+    plt.ylabel('Elevation (m)')
+    plt.title('Race Elevation Profile (1m resolution)')
+    plt.legend()
+    plt.grid(True)
+    plt.show()
+
+    plt.figure(figsize=(12, 4))
+    plt.plot(dists, elevs, 'o', color='red', label='Original GPX Points')
+    plt.plot(new_dists, new_elevs, '-', color='blue', label='Interpolated (1m)')
+    plt.xlabel('Distance (m)')
+    plt.ylabel('Elevation (m)')
+    plt.title('Original vs. Interpolated Elevation')
+    plt.legend()
+    plt.grid(True)
+    plt.show()
